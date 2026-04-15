1020a0b0b17afc9653a39cc724477f7a3fc41bb4
axelpolleux <axelpolleux@proton.me>
2026-04-15 16:23:29 +0200

    feat(main): add `init_env` and `exit_shell` functions
    - Implemented `init_env` to initialize environment variables.
    - Added `exit_shell` for handling shell exit logic.

--- src/main.c
+++ src/main.c
@@ -6,11 +6,49 @@
 /*   By: ethutin- <ethutin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 11:53:00 by ethutin-          #+#    #+#             */
-/*   Updated: 2026/04/08 16:47:44 by apolleux         ###   ########.fr       */
+/*   Updated: 2026/04/15 16:23:06 by apolleux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+
+void	init_env(t_data *data, char **env)
+{
+	t_env	*new;
+	int		i;
+
+	i = -1;
+	if (!env || !(*env))
+	{
+		free_data(data);
+		exit(0);
+	}
+	while (env[++i])
+	{
+		new = new_env(ft_strdup(env[i]), 1);
+		if (!new)
+			data_malloc_error(data);
+		add_to_bottom(&data->t_env, new);
+	}
+}
+
+int	exit_shell(char *line)
+{
+	if (!line)
+	{
+		printf("exit\n");
+		return (1);
+	}
+	if (line && *line && !full_void(line))
+		add_history(line);
+	if (!strcmp(line, "exit"))
+	{
+		printf("exit\n");
+		return (1);
+	}
+	return (0);
+}
 
 int	main(void)
 {
